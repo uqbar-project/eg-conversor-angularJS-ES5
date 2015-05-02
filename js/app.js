0c3e034fa00029a3f4107f764ca9fb3a03a38f10
Nicolas Passerini <npasserini@gmail.com>
2015-05-02 23:56:49 +0000

    Agrego paso 2

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ var app = angular.module('conversorApp', [])
 
 /* Controllers */
 app.controller('ConversorController', function ($scope) {
-	/* scope */
-	$scope.millas = 0;
-	$scope.kilometros = ' <ingrese millas> '
-		
-	$scope.convertir = function() {
-		$scope.kilometros = $scope.millas * 1.60934
-	}
+	$scope.conversor = new Conversor();
 });
   
+function Conversor()  {
+	this.millas = 0;
+	this.kilometros = function() {
+		return this.millas * 1.60934
+	}	
+}
   

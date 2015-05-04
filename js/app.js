41dc72349f7a07e6fa9ce76dbf793f8a4c221ff0
Nicolas Passerini <npasserini@gmail.com>
2015-05-04 01:41:04 +0000

    Agrego validaciones en el paso 4

--- js/app.js
+++ js/app.js
@@ -8,10 +8,13 @@ app.controller('ConversorController', function ($scope) {
 });
   
 function Conversor()  {
-	this.millas = 0;
-	this.kilometros = '';
+	this.millas = '';
+	this.kilometros = NaN;
 	this.convertir = function() {
 		this.kilometros =  this.millas * 1.60934
 	}	
+
+	this.isValid = function(form) {
+		return !isNaN(this.kilometros);
+	}
 }
-  

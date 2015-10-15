77d5f2508655d823a846d003a3037e168fef271c
Fernando Dodino <fernando.dodino@gmail.com>
2015-10-15 16:24:17 +0000

    Actualizamos a release 0.1.4 de xtrest

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,8 +3,8 @@
 var app = angular.module('conversorApp', [])
 
 /* Controllers */
-app.controller('ConversorController', function ($scope, Conversor) {
-	$scope.conversor = new Conversor();
+app.controller('ConversorController', function (Conversor) {
+	return new Conversor();
 });
   
 app.factory('Conversor', function($http) {
@@ -12,13 +12,15 @@ app.factory('Conversor', function($http) {
 		this.millas = NaN;
 		this.kilometros = NaN;
 		this.convertir = function() {
-			if(this.isValid())
-	      $http.get('/convertir/' + this.millas)
-	      		.success(function(result) { this.kilometros = result.kilometros; }.bind(this));
+			this.kilometros = NaN;
+			if(this.isValid()) {
+			      $http.get('/convertir/' + this.millas)
+		      		.success(function(result) { this.kilometros = result.kilometros; }.bind(this));
+			}
 		}	
 
 		this.isValid = function(form) {
-			return !isNaN(this.kilometros);
+			return !isNaN(this.millas);
 		}
 	}
 });

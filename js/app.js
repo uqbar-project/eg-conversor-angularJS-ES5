5fb90dd608f2ca685418d3ec71b8d903c9eec804
fdodino <fernando.dodino@gmail.com>
2016-09-22 08:28:16 -0300

    Fixing missing ;

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 'use strict';
 
-var app = angular.module('conversorApp', [])
+var app = angular.module('conversorApp', []);
 
 /* Controllers */
-app.controller('conversorCtrl', function () {
-	/* scope */
-	this.millas = 0;
-	this.kilometros = ' <ingrese millas> '
-		
-	this.convertir = function() {
-		this.kilometros = this.millas * 1.60934
-	}
+app.controller('conversorCtrl', function() {
+    /* scope */
+    this.millas = 0;
+    this.kilometros = ' <ingrese millas> ';
+
+    this.convertir = function() {
+        this.kilometros = this.millas * 1.60934;
+    };
 });
-  
-  

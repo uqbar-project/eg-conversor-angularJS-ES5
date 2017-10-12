cbcd8c98a7a706e3d66425b7880778e2335ad9cd
Martin Dias <tinchodias@gmail.com>
2017-10-12 13:24:53 -0300

    Simplifico y cambio de  a controller as

--- js/app.js
+++ js/app.js
@@ -2,15 +2,9 @@
 
 var app = angular.module('conversorApp', [])
 
-/* Controllers */
-app.controller('ConversorController', function ($scope) {
-	$scope.conversor = new Conversor();
-});
-  
-function Conversor()  {
+app.controller('ConversorController', function () {
 	this.millas = 0;
 	this.kilometros = function() {
 		return this.millas * 1.60934
-	}	
-}
-  
+	}
+});
